98b8c9d25202613c57221c12529595376a067df1
aheitzen <alexaheitzenrader@yahoo.com>
2016-06-10 12:52:57 -0700

    parallex

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ console.log("I work");
 
 
 $(document).ready(function() {
-	$('.parallax-window').parallax({imageSrc: 'images/copmuter.jpeg'});
+	$('.parallax-window').parallax({imageSrc: 'images/computer.jpeg'});
+});
 		
